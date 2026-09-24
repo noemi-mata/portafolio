c9fa608ef33c40d2d7bcacf6c7e143869e721273
Noemi Mata Ruiz <1225100341.nmr@gmail.com>
2026-09-24 15:51:19 -0600

    Actualizacion de recursividad imprimiendome 10 veces saludo

--- datos/unidad1/recursividad/Recursividad.java
+++ datos/unidad1/recursividad/Recursividad.java
@@ -1,18 +1,23 @@
 package datos.unidad1.recursividad;
+import java.util.Scanner;
 
 public class Recursividad {
 		
-	public static void saludo(int total){
-		
+	public static void saludo(int total, String nombre){
+
 		if(total <= 0) //condicion base
 			return;
 		else{	//condicion recrsiva o repetitiva
-			System.out.println("Hola");			
-			saludo(total -1);
+			System.out.println("Hola"+ " " + nombre);			
+			saludo(total -1,nombre);
 		}
 	}
 
 	public static void main(String[] args){
-		saludo(10);
+		Scanner sc = new Scanner(System.in);
+
+        	System.out.print("Ingresa un nombre: ");
+        	String nombre = sc.nextLine(); 
+		saludo(10, nombre);
 	}
 }
