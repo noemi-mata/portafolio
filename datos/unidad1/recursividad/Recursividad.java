3af1e4032d2e4627b74a7d0f4ab1a839746422ca
Noemi Mata Ruiz <1225100341.nmr@gmail.com>
2026-09-25 13:58:35 -0600

    Se añadio el Ejercicio de Cuenta regresiva

--- datos/unidad1/recursividad/Recursividad.java
+++ datos/unidad1/recursividad/Recursividad.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 
 public class Recursividad {
 		
-	public static void saludo(int total, String nombre){
+	/*public static void saludo(int total, String nombre){
 
 		if(total <= 0) //condicion base
 			return;
@@ -11,13 +11,31 @@ public class Recursividad {
 			System.out.println("Hola"+ " " + nombre);			
 			saludo(total -1,nombre);
 		}
+	}*/
+
+	public static void cuentaRegresiva(int n){
+
+		if(n < 1){
+			return;
+		} else {
+			System.out.println(n + " ");
+			cuentaRegresiva(n-1);
+		}
 	}
 
+	/**
+	*Funcion que realiza cuenta regresiva de acuerdo a un 
+	*valor entero
+	*Fecha: 25 de Septiembre
+	*Autor: Noemi Mata
+	*/
 	public static void main(String[] args){
-		Scanner sc = new Scanner(System.in);
+		//Scanner sc = new Scanner(System.in);
 
-        	System.out.print("Ingresa un nombre: ");
-        	String nombre = sc.nextLine(); 
-		saludo(10, nombre);
+        	//System.out.print("Ingresa un nombre: ");
+        	//String nombre = sc.nextLine(); 
+		//saludo(10, nombre);
+
+		cuentaRegresiva(100);
 	}
 }
